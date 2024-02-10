002fe42dc1814abdbfc33419ddcba2669ef1e83d
maheshkaria <33889399+maheshkaria@users.noreply.github.com>
2024-02-10 22:11:30 +0530

    day 2 data type examples

--- 2/day2_exp.py
+++ 2/day2_exp.py
@@ -9,11 +9,6 @@ print(type(True))                       # it will show type bool
 # Function is something helps to run same code again and again by calling them with distinct name
 # print(), len() and type() all of them are some examples of function which we have used so far
 
-
-
-
-
-
-
-
+a = 123
+print("value of a: " + str(a))  # we can type cast variable from any data type to any other data type as shown
 
