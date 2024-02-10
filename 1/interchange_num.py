2ba570d4be63477c2d8071f5f08c562e1c75ccba
maheshkaria <33889399+maheshkaria@users.noreply.github.com>
2024-02-10 21:29:22 +0530

    user input example

---
+++ 1/interchange_num.py
@@ -0,0 +1,16 @@
+# interchange two numbers which are taken as user input
+
+a = eval(input("enter number for a: "))
+b = eval(input("enter number for b: "))
+
+#####
+
+t = a
+a = b
+b = t
+
+#####
+
+print("a: %d" % a)
+print("b: %d" % b)
+
