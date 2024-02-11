be049d72cbd49a5054e0cf2cbef475d04c1d3ef1
maheshkaria <33889399+maheshkaria@users.noreply.github.com>
2024-02-12 03:33:05 +0530

    day 4 tutorials

---
+++ 4/day4_exp.py
@@ -0,0 +1,10 @@
+import random
+# we can import standard module or user defined modules using import statement
+# module is package or collection of related code
+
+# we can generate random int using randint function
+print(random.randint(1, 100))
+
+# we can generate random float using random function
+print(random.random())
+
---
+++ 4/describe.md
@@ -0,0 +1,3 @@
+Day 4 - Topics
+- Randomization
+- Lists
