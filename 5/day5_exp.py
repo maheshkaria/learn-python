414e62661fe520b6b5f8c848e0d84d09ab7ef985
maheshkaria <33889399+maheshkaria@users.noreply.github.com>
2024-02-12 04:02:48 +0530

    day 4 tutorials

---
+++ 5/day5_exp.py
@@ -0,0 +1,8 @@
+# Sometimes we want to execute same piece of code multiple times or iterate over list at that time we can use loops
+
+# For loop
+items = ["item1", "item2", "item3"]
+
+for each_item in items:
+    print(each_item)
+
