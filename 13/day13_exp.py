f48c131b0f8ab65b46be9b2346241b5d674bcb8c
maheshkaria <33889399+maheshkaria@users.noreply.github.com>
2024-02-12 17:29:51 +0530

    day 13 tutorials

--- 13/day13_exp.py
+++ 13/day13_exp.py
@@ -2,7 +2,7 @@
 # First describe bug then think what all, ways it can be produced and then try to come up with solution
 # Try to reproduce bug and make long term fix
 # If none of them works add break point check with exact inputs and see what is wrong and apply fix
-# Evaluating each line is good if project is too big to run limited scope of project
+# Evaluating each line using debugger is good if project is too big to run limited scope of project
 # Debugging skill takes time to build and comes with confidence over a period so give yourself time
 # we can use logger or print for debugging
 ###############################################################
