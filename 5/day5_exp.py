650df8622e47f98fa98479f4a7550967f7005d09
maheshkaria <33889399+maheshkaria@users.noreply.github.com>
2024-02-12 13:10:45 +0530

    day 5 tutorials

--- 5/day5_exp.py
+++ 5/day5_exp.py
@@ -7,3 +7,17 @@ for each_item in items:
     print(each_item)
 
 print(items)
+
+#############################################
+# range function is used to iterate over certain range of number with provided step size
+
+# following block will print odd numbers
+for i in range(1, 100, 2):
+    print(i)
+    # 1, 3, 5, 7.. and so on till 99
+
+#############################################
+# code block piece of code that needs to be executed together when control moves there
+
+
+
