0439265960a0cc318b63eabd63b752fe14a74247
maheshkaria <33889399+maheshkaria@users.noreply.github.com>
2024-02-14 23:59:35 +0530

    day 16 tutorials

--- 16/day16_exp.py
+++ 16/day16_exp.py
@@ -1,1 +1,4 @@
 ###############################################################
+# Earlier we had procedural programming where execution used to happen sequentially but with complex problems
+# there was need to better way to solve programming problems and OOPs came as one of the solution
+###############################################################
