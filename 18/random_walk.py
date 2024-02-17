ddf6edcbb847603e930b6c0a0e67675f5237c183
maheshkaria <33889399+maheshkaria@users.noreply.github.com>
2024-02-18 01:06:24 +0530

    day 18 tutorials

--- 18/random_walk.py
+++ 18/random_walk.py
@@ -1,22 +1,16 @@
 from turtle import Turtle, Screen, colormode
-from random import randint
+from random import randint, choice
 t = Turtle()
 colormode(255)
 t.pensize(10)
 t.speed(100)
 
-while True:
-    val = randint(1, 255) % 2
-    t.pencolor(randint(0, 255), randint(0, 255), randint(0, 255))
-    if val:
-        t.forward(30)
-    else:
-        t.backward(30)
+angles = [0, 90, 180, 270]
 
-    if val:
-        t.setheading(90)
-    else:
-        t.setheading(180)
+while True:
+    t.pencolor(randint(0, 255), randint(0, 255), randint(0, 255))
+    t.forward(30)
+    t.setheading(choice(angles))
 
 
 
