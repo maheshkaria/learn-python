49e293806798b27d0c4378a2b9770fe713d3fd23
maheshkaria <33889399+maheshkaria@users.noreply.github.com>
2024-02-17 21:18:28 +0530

    day 17 tutorials

--- 17/day17_exp.py
+++ 17/day17_exp.py
@@ -4,3 +4,20 @@
 # attributes are nothing but variables
 # methods are nothing but functions related to class
 #################################################################
+
+# Following is example of class
+class Car:
+    color: str = ""
+    name: str = ""
+
+    def __init__(self, color, name):
+        self.color = color
+        self.name = name
+
+    def __repr__(self):
+        return "color is " + self.color
+
+# following is example of creation of object
+my_car = Car("red", "model X")
+
+print(my_car)
