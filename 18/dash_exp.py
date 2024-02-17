2243a6b15bee26ea6b3434f47c4a4468c2d5a687
maheshkaria <33889399+maheshkaria@users.noreply.github.com>
2024-02-18 00:02:36 +0530

    day 18 tutorials

---
+++ 18/dash_exp.py
@@ -0,0 +1,14 @@
+from turtle import Turtle, Screen
+
+t = Turtle()
+
+
+while True:
+    t.forward(10)
+    t.pencolor("white")
+    t.forward(10)
+    t.pencolor("black")
+
+
+scr = Screen()
+scr.exitonclick()
