bcd62d8c9c7241f27b900f1a94be8588930b9749
maheshkaria <33889399+maheshkaria@users.noreply.github.com>
2024-02-17 21:15:59 +0530

    day 17 tutorials

---
+++ 17/day17_exp.py
@@ -0,0 +1,6 @@
+#################################################################
+# In OOPs we model some real life entity to a class aka blueprint
+# every class has some attributes and methods
+# attributes are nothing but variables
+# methods are nothing but functions related to class
+#################################################################
---
+++ 17/describe.md
@@ -0,0 +1,2 @@
+Day 17 - Topics
+- How to use OOPs ?
