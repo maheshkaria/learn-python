0f2c65ca4a81c7b32ff6d5c5eee54b8a57d4a698
maheshkaria <33889399+maheshkaria@users.noreply.github.com>
2024-02-17 23:57:01 +0530

    day 18 tutorials

--- 18/day18_exp.py
+++ 18/day18_exp.py
@@ -1,6 +1,8 @@
 from turtle import Turtle, Screen
 
 tim = Turtle()
+tim2 = Turtle()
+tim3 = Turtle()
 
 tim.shape("arrow")
 tim.color("red")
@@ -9,6 +11,12 @@ counter = 100
 while True:
     tim.forward(counter)
     tim.right(90)
+
+    tim2.forward(counter)
+    tim2.right(90)
+    tim3.backward(counter)
+    tim3.left(90)
+
 
 screen = Screen()
 screen.exitonclick()
