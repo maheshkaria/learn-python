75c5613188e2705f594b28a05994c868471a606a
maheshkaria <33889399+maheshkaria@users.noreply.github.com>
2024-02-18 13:54:36 +0530

    day 19 tutorials

--- 19/sketch_app.py
+++ 19/sketch_app.py
@@ -3,11 +3,36 @@ from turtle import Turtle, Screen
 tim = Turtle()
 scr = Screen()
 
+
 def move_forwards():
     tim.forward(10)
 
-scr.listen()
-scr.onkey(key="space", fun=move_forwards)
 
+def move_backwards():
+    tim.backward(10)
+
+
+def clear_screen():
+    tim.reset()
+
+
+def move_clockwise():
+    for _ in range(10):
+        tim.setheading(tim.heading() + 1)
+        tim.forward(1)
+
+
+def move_anti_clockwise():
+    for _ in range(10):
+        tim.setheading(tim.heading() - 1)
+        tim.forward(1)
+
+
+scr.listen()
+scr.onkey(key="W", fun=move_forwards)
+scr.onkey(key="S", fun=move_backwards)
+scr.onkey(key="A", fun=move_clockwise)
+scr.onkey(key="D", fun=move_anti_clockwise)
+scr.onkey(key="C", fun=clear_screen)
 
 scr.exitonclick()
