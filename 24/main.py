15e4181fd0e417cef1643598df9b2aa128fe7f23
maheshkaria <33889399+maheshkaria@users.noreply.github.com>
2024-03-18 00:45:47 +0530

    path examples to read files

--- 24/main.py
+++ 24/main.py
@@ -6,3 +6,11 @@ with open("my_file.txt", "w") as my_file:   # write mode
 
 with open("my_file.txt", "a") as my_file:   # append mode
     my_file.write("\nmy new content")
+
+# absolute path is path w.r.t. root of system
+with open("C:\\Users\\pc\\PycharmProjects\\learn-python\\24\\random_folder\\file1.txt") as read_file:
+    print(read_file.read())
+
+# relative path is path w.r.t. program from where it is running
+with open(".\\random_folder\\file2.txt") as read_file:
+    print(read_file.read())
