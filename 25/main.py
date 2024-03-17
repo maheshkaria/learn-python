303006756070da4b607045da099725b46e2ad2a7
maheshkaria <33889399+maheshkaria@users.noreply.github.com>
2024-03-18 01:23:11 +0530

    pandas example

--- 25/main.py
+++ 25/main.py
@@ -18,10 +18,32 @@
 # It supports 1-D series and 2-D dataframe data types
 # Series is basically list while Dataframe is tabular representation which has row and column
 
-
 import pandas as pd
 
 df = pd.read_csv("weather_data.csv")
+
 print(df["temp"])
+
 print(df)
+
 print(type(df))
+
+print(df.to_dict())     # we can convert it to dict
+
+print(type(df["temp"]))     # each column in pandas DF is basically Series
+print(df["temp"].to_list())     # we can convert any Series object to python list using to_list()
+
+print(df["temp"].sum()/df["temp"].count())      # average temp
+
+print(df["temp"].mean())    # average temp using mean function
+
+print(df["temp"].max())     # get max value
+
+print(df["condition"])
+
+print(df.condition)     # we can use dot notation with pandas dataframe like this
+
+print(df[df.day == "Monday"])
+
+print(df[df.temp == df.temp.max()])
+
