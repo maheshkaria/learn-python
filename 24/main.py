a425bb9f042b735ef984443e66dd7f4b05e72150
maheshkaria <33889399+maheshkaria@users.noreply.github.com>
2024-03-18 00:31:51 +0530

    Added day 24 changes

--- 24/main.py
+++ 24/main.py
@@ -1,7 +1,8 @@
-from turtle import Screen, Turtle
+# with open("my_file.txt") as my_file:   # read mode
+#     print(my_file.read())
 
-screen = Screen()
-screen.setup(width=600, height=600)
-screen.bgcolor("black")
-screen.title("My snake game!")
-screen.exitonclick()
+with open("my_file.txt", "w") as my_file:   # write mode
+    my_file.write("my content")
+
+with open("my_file.txt", "a") as my_file:   # append mode
+    my_file.write("\nmy new content")
