05678964dfcc25f30ea79b9446aec4d082bf7730
maheshkaria <33889399+maheshkaria@users.noreply.github.com>
2024-03-18 01:11:44 +0530

    pandas example

--- 25/main.py
+++ 25/main.py
@@ -15,8 +15,13 @@
 # Pandas library helps in data analysis
 # we can install pandas using 'pip install pandas'
 
+# It supports 1-D series and 2-D dataframe data types
+# Series is basically list while Dataframe is tabular representation which has row and column
+
+
 import pandas as pd
 
 df = pd.read_csv("weather_data.csv")
 print(df["temp"])
 print(df)
+print(type(df))
