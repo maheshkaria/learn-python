d0d5affe910ce33bd130129c5e42e272472276f4
maheshkaria <33889399+maheshkaria@users.noreply.github.com>
2024-03-28 00:31:20 +0530

    comprehension example

---
+++ 26/main.py
@@ -0,0 +1,18 @@
+# how to create list using list comprehensions
+
+# list comprehension is created using another iterator such as set/list/tuple/string
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# unconditional list comprehension
+new_list = [item * item for item in my_list]
+
+print(new_list)
+
+# conditional list comprehension
+new_list = [item * item for item in my_list if item % 2 == 0]
+
+print(new_list)
+
+
+
+
