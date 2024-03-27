05d16c09386189c1bd030fc36ccc15b9f965acfb
maheshkaria <33889399+maheshkaria@users.noreply.github.com>
2024-03-28 00:35:28 +0530

    comprehension example

--- 26/main.py
+++ 26/main.py
@@ -1,6 +1,6 @@
 # how to create list using list comprehensions
 
-# list comprehension is created using another iterator such as set/list/tuple/string
+# list comprehension is created using another sequence such as set/list/tuple/string
 my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 # unconditional list comprehension
@@ -13,6 +13,8 @@ new_list = [item * item for item in my_list if item % 2 == 0]
 
 print(new_list)
 
+# conditional list comprehension -> print only even numbers
+new_list = [item for item in my_list if item % 2 == 0]
 
-
+print(new_list)
 
